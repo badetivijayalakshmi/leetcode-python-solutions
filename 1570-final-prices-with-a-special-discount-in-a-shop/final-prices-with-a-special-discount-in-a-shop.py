b7c532f50f1943d2451aa517c8eda0074bac833d
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2026-01-14 23:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (8.46%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,8 @@
+class Solution:
+        def finalPrices(self, A):
+            stack = []
+            for i, a in enumerate(A):
+                while stack and A[stack[-1]] >= a:
+                    A[stack.pop()] -= a
+                stack.append(i)
+            return A
