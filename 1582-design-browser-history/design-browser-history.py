b6d5db3b94a260d72ba3254006c7637b126971db
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2026-03-08 16:16:35 +0530

    Time: 39 ms (54.16%) | Memory: 22.3 MB (20.76%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,28 +1,32 @@
+class Node:
+    def __init__(self,url):
+        self.url = url
+        self.prev = None
+        self.next = None
+
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.current = homepage
-        self.forwardStack = []
-        self.backStack = []
+        self.current = Node(homepage)
         
     def visit(self, url: str) -> None:
-        self.backStack.append(self.current)
-        self.current = url
-        self.forwardStack = []        
+        new_node = Node(url)
+        self.current.next = new_node
+        new_node.prev = self.current
+
+        self.current = new_node        
 
     def back(self, steps: int) -> str:
-        while steps > 0 and self.backStack:
-            self.forwardStack.append(self.current)
-            self.current = self.backStack.pop()
+        while steps > 0 and self.current.prev:
+            self.current = self.current.prev
             steps -= 1
-        return self.current
+        return self.current.url
 
     def forward(self, steps: int) -> str:
-        while steps > 0 and self.forwardStack:
-            self.backStack.append(self.current)
-            self.current = self.forwardStack.pop()
+        while steps > 0 and self.current.next:
+            self.current = self.current.next
             steps -= 1
-        return self.current
+        return self.current.url
         
 
 
