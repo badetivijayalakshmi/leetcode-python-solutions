1162b44c709f2c05f44cb2a3983f8de50e9697b5
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2026-03-15 12:59:20 +0530

    Time: 195 ms (19.86%) | Memory: 19.9 MB (63.93%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -8,10 +8,9 @@ class Solution:
         for i in range(len(s)):
             window[s[i]] += 1
             if i >= k:
-                if window[s[i-k]] == 1:
+                window[s[i-k]] -= 1
+                if window[s[i-k]] == 0:
                     del window[s[i-k]]
-                else:
-                    window[s[i-k]] -= 1
             if window == count:
                 res.append(i-k+1)
         return res   
