6a42511b5dbe5d7ca387f199d9a74d30bfee02e5
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2026-03-23 12:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (95.96%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,24 +1,41 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, a: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        c0 = c1 = c2 = 0
-        x = 0
-        n = len(nums)
-        for i in range(n):
-            if nums[i] == 0:
-                c0 += 1
-            elif nums[i] == 1:
-                c1 += 1
-            else:
-                c2 += 1
-        for _ in range(c0):
-            nums[x] = 0
-            x += 1
-        for _ in range(c1):
-            nums[x] = 1
-            x += 1
-        for _ in range(c2):
-            nums[x] = 2
-            x += 1
+        #c0 = c1 = c2 = 0
+        #x = 0
+        #n = len(nums)
+        #for i in range(n):
+        #    if nums[i] == 0:
+        #        c0 += 1
+        #    elif nums[i] == 1:
+        #        c1 += 1
+        #    else:
+        #        c2 += 1
+        #for _ in range(c0):
+        #    nums[x] = 0
+        #    x += 1
+        #for _ in range(c1):
+        #    nums[x] = 1
+        #    x += 1
+        #for _ in range(c2):
+        #    nums[x] = 2
+        #    x += 1
+#Dutch National Flag algo
+        low = 0
+        mid = 0
+        high = len(a) - 1
+        
+        while mid <= high:
+            if a[mid] == 0:
+                a[low], a[mid] = a[mid], a[low]
+                low += 1
+                mid += 1
+        
+            elif a[mid] == 1:
+                mid += 1
+        
+            else:  # a[mid] == 2
+                a[mid], a[high] = a[high], a[mid]
+                high -= 1
