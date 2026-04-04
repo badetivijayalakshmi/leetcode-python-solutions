703f7e493f9fe171af57213ddef26074f7898cb3
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2026-04-04 19:25:04 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (50.85%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        freq = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[j]==target-nums[i]:
-                    return[i,j]
+            diff = target - nums[i]
+            if diff in freq:
+                return [freq[diff],i]
+            freq[nums[i]] = i
+        return []
+        
